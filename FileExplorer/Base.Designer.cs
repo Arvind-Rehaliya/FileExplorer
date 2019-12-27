33d90cda232618385b03424a9ee1d1f7e3b6e2d1
arvind <arvind@no-mail.com>
2019-12-28 00:23:59 +0530

    Fixed Resizing Issues of Panels

--- FileExplorer/Base.Designer.cs
+++ FileExplorer/Base.Designer.cs
@@ -46,7 +46,7 @@
             treeNode8});
             this.pn_flow = new System.Windows.Forms.FlowLayoutPanel();
             this.lb_info = new System.Windows.Forms.Label();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.sp_container = new System.Windows.Forms.SplitContainer();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.tv_favorites = new System.Windows.Forms.TreeView();
             this.tv_libraries = new System.Windows.Forms.TreeView();
@@ -59,10 +59,10 @@
             this.bt_next = new System.Windows.Forms.Button();
             this.tb_search = new System.Windows.Forms.TextBox();
             this.pn_flow.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sp_container)).BeginInit();
+            this.sp_container.Panel1.SuspendLayout();
+            this.sp_container.Panel2.SuspendLayout();
+            this.sp_container.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -78,8 +78,7 @@
             this.pn_flow.Location = new System.Drawing.Point(0, 0);
             this.pn_flow.Margin = new System.Windows.Forms.Padding(10);
             this.pn_flow.Name = "pn_flow";
-            this.pn_flow.Padding = new System.Windows.Forms.Padding(5);
-            this.pn_flow.Size = new System.Drawing.Size(672, 417);
+            this.pn_flow.Size = new System.Drawing.Size(713, 461);
             this.pn_flow.TabIndex = 0;
             this.pn_flow.Click += new System.EventHandler(this.pn_flow_Clicked);
             this.pn_flow.Paint += new System.Windows.Forms.PaintEventHandler(this.pn_flow_Paint);
@@ -90,37 +89,39 @@
             // 
             // lb_info
             // 
-            this.lb_info.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_info.BackColor = System.Drawing.Color.Black;
             this.lb_info.ForeColor = System.Drawing.Color.White;
-            this.lb_info.Location = new System.Drawing.Point(8, 5);
+            this.lb_info.Location = new System.Drawing.Point(0, 0);
+            this.lb_info.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
             this.lb_info.Name = "lb_info";
-            this.lb_info.Size = new System.Drawing.Size(652, 23);
+            this.lb_info.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.lb_info.Size = new System.Drawing.Size(696, 28);
             this.lb_info.TabIndex = 0;
-            this.lb_info.Text = "This folder is empty.";
+            this.lb_info.Text = "This folder is empty";
             this.lb_info.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lb_info.UseWaitCursor = true;
+            this.lb_info.Visible = false;
             // 
-            // splitContainer1
+            // sp_container
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
+            this.sp_container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sp_container.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.sp_container.Location = new System.Drawing.Point(0, 0);
+            this.sp_container.Name = "sp_container";
             // 
-            // splitContainer1.Panel1
+            // sp_container.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.flowLayoutPanel1);
-            this.splitContainer1.Panel1MinSize = 65;
+            this.sp_container.Panel1.Controls.Add(this.flowLayoutPanel1);
+            this.sp_container.Panel1MinSize = 65;
             // 
-            // splitContainer1.Panel2
+            // sp_container.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.pn_flow);
-            this.splitContainer1.Panel2MinSize = 100;
-            this.splitContainer1.Size = new System.Drawing.Size(824, 417);
-            this.splitContainer1.SplitterDistance = 150;
-            this.splitContainer1.SplitterWidth = 2;
-            this.splitContainer1.TabIndex = 1;
+            this.sp_container.Panel2.Controls.Add(this.pn_flow);
+            this.sp_container.Panel2.SizeChanged += new System.EventHandler(this.sp_container_Panel2_SizeChanged);
+            this.sp_container.Panel2MinSize = 110;
+            this.sp_container.Size = new System.Drawing.Size(865, 461);
+            this.sp_container.SplitterDistance = 150;
+            this.sp_container.SplitterWidth = 2;
+            this.sp_container.TabIndex = 1;
             // 
             // flowLayoutPanel1
             // 
@@ -133,7 +134,7 @@
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(150, 417);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(150, 461);
             this.flowLayoutPanel1.TabIndex = 0;
             this.flowLayoutPanel1.WrapContents = false;
             // 
@@ -214,10 +215,10 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Controls.Add(this.sp_container);
             this.panel1.Location = new System.Drawing.Point(0, 42);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(824, 417);
+            this.panel1.Size = new System.Drawing.Size(865, 461);
             this.panel1.TabIndex = 0;
             // 
             // tb_path
@@ -231,7 +232,7 @@
             this.tb_path.Location = new System.Drawing.Point(97, 10);
             this.tb_path.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
             this.tb_path.Name = "tb_path";
-            this.tb_path.Size = new System.Drawing.Size(466, 21);
+            this.tb_path.Size = new System.Drawing.Size(507, 21);
             this.tb_path.TabIndex = 8;
             this.tb_path.WordWrap = false;
             this.tb_path.Enter += new System.EventHandler(this.tb_path_Enter);
@@ -250,7 +251,7 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(824, 39);
+            this.panel2.Size = new System.Drawing.Size(865, 39);
             this.panel2.TabIndex = 0;
             // 
             // button2
@@ -261,7 +262,7 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Image = global::FileExplorer.Properties.Resources.search_204;
-            this.button2.Location = new System.Drawing.Point(785, 6);
+            this.button2.Location = new System.Drawing.Point(826, 6);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(27, 28);
             this.button2.TabIndex = 12;
@@ -277,7 +278,7 @@
             this.bt_refresh.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
             this.bt_refresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt_refresh.Image = global::FileExplorer.Properties.Resources.refresh_25;
-            this.bt_refresh.Location = new System.Drawing.Point(569, 7);
+            this.bt_refresh.Location = new System.Drawing.Point(610, 7);
             this.bt_refresh.Name = "bt_refresh";
             this.bt_refresh.Size = new System.Drawing.Size(27, 28);
             this.bt_refresh.TabIndex = 11;
@@ -314,7 +315,7 @@
             this.tb_search.BackColor = System.Drawing.Color.DimGray;
             this.tb_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_search.ForeColor = System.Drawing.Color.White;
-            this.tb_search.Location = new System.Drawing.Point(641, 10);
+            this.tb_search.Location = new System.Drawing.Point(682, 10);
             this.tb_search.Name = "tb_search";
             this.tb_search.Size = new System.Drawing.Size(141, 21);
             this.tb_search.TabIndex = 1;
@@ -324,16 +325,19 @@
             // 
             // Base
             // 
-            this.ClientSize = new System.Drawing.Size(824, 459);
+            this.ClientSize = new System.Drawing.Size(865, 503);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(450, 0);
             this.Name = "Base";
+            this.Load += new System.EventHandler(this.Base_Load);
+            this.SizeChanged += new System.EventHandler(this.Base_SizeChanged);
             this.pn_flow.ResumeLayout(false);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel1.PerformLayout();
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
+            this.sp_container.Panel1.ResumeLayout(false);
+            this.sp_container.Panel1.PerformLayout();
+            this.sp_container.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sp_container)).EndInit();
+            this.sp_container.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
@@ -345,7 +349,7 @@
         #endregion
 
         public System.Windows.Forms.FlowLayoutPanel pn_flow;
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer sp_container;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
